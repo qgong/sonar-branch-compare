b5bffd010f2878adfc9e856a06ed38a8e68a3ac4
jizhao <jizhao@redhat.com>
2014-04-03 18:27:55 +0800

    Use ajax in template

--- app/controllers/branch_comparison_controller.rb
+++ app/controllers/branch_comparison_controller.rb
@@ -28,26 +28,29 @@ class BranchComparisonController < ApplicationController
     # find other branches
     target_branches = self._get_branches(base_branch.key, target_branch_names)
     # find snapshots
-    @target_projects = {}
+    @target_projects = []
     [base_branch].concat(target_branches).each do |project|
-      data = {:name => project.name(true),
+      data = {:id => project.id,
+              :name => project.name(true),
               :key => project.key,
-              :snapshots => self._get_latest_snapshots(project.id)}
+              :snapshots => self._get_latest_snapshot(project.id)}
       data[:versions] = data[:snapshots].keys.sort do |a, b|
         a.to_f <=> b.to_f
       end.reverse
-      unless data[:snapshots].empty?
-        @target_projects[project.id] = data
-      end
+      @target_projects.push(data)
     end
     # find enabled metrics
     @metrics = Metric.all.select {|metric| metric.enabled}
     # remove metrics with no values
     @metrics = @metrics.select do |metric|
       result = false
-      @target_projects.each_pair do |project_id, data|
+      @target_projects.each do |data|
         data[:snapshots].each_pair do |version, snapshot|
-          if snapshot.measure(metric)
+          value = snapshot.measure(metric)
+          if value.is_a?(String) and not value.empty?
+            result = true
+            break
+          elsif not value.nil?
             result = true
             break
           end
@@ -56,8 +59,10 @@ class BranchComparisonController < ApplicationController
       end
       result
     end
-    @base_project = @target_projects.delete(base_branch.id)
+    @base_project = @target_projects.delete_at(0)
     @metrics.sort! {|a, b| a.name <=> b.name}
+
+    #render :json => @base_project
 
     rescue => e
       render :text => e
@@ -68,8 +73,7 @@ class BranchComparisonController < ApplicationController
   def _get_branches(project_key, target_branches=nil)
     # get project key(without branch name)
     project_key_without_branch = project_key.split(':')[0,2].join(':')
-    sql_template = "kee LIKE :key_pattern AND kee != :base_branch_key
-                    AND root_id IS NULL"
+    sql_template = "kee LIKE :key_pattern AND kee != :base_branch_key"
     sql_params = {:key_pattern => "#{project_key_without_branch}%",
                   :base_branch_key => project_key}
     if target_branches and not target_branches.empty?
@@ -80,32 +84,41 @@ class BranchComparisonController < ApplicationController
     end
     # filter projects
     target_branches = Project.all(:conditions => [sql_template, sql_params],
-                                  :order => "kee")
+                                  :order => "kee ASC")
     return target_branches
   end
 
-  # get latest snapshots of different versions
-  def _get_latest_snapshots(project_id)
-    hash = {}
-    snapshots = Snapshot.all(:conditions => ['project_id = ?', project_id])
-    snapshots.each do |snapshot|
-      unless hash[snapshot.version]
-        hash[snapshot.version] = []
-      end
-      hash[snapshot.version].push(snapshot)
+  # get latest snapshot of a specific version
+  def _get_latest_snapshot(project_id, version)
+    snapshot = Snapshot.first(:conditions => ['project_id = ? AND version = ?',
+                                              project_id, version],
+                              :order => 'created_at DESC')
+    return snapshot
+  end
+
+  def get_project
+    # search for project
+    project = Project.by_key(params['project_id'].to_i)
+    if project.nil?
+      render :json => {'status' => false,
+                      'error' => "Project #{params['project_id']} not found"}
+      return
     end
-    # find the latest one
-    hash.each_pair do |version, snapshots|
-      latest_time = Time.at(0)
-      latest_snapshot = nil
-      snapshots.each do |snapshot|
-        if snapshot.created_at > latest_time
-          latest_time = snapshot.created_at
-          latest_snapshot = snapshot
-        end
+    # find snapshot
+    snapshot = self._get_latest_snapshot(params['project_id'].to_i, params['version'])
+    # calculate result for each metric
+    json = {'name' => project.name(true), 'metrics' => {},
+            'status' => true}
+    params['metrics'].each do |metric_id|
+      metric = Metric.by_id(metric_id)
+      if metric
+        json['metrics'][metric_id] = snapshot.measure(metric_id)
+      else
+        render :json => {'status' => false,
+                        'error' => "Metric #{metric_id} not found"}
+        return
       end
-      hash[version] = latest_snapshot
     end
-    return hash
+    render :json => json
   end
 end
