c25d232c01fd2222072c0bea64501c93da9ba3ed
jizhao <jizhao@redhat.com>
2014-04-11 16:15:31 +0800

    Rename template file

--- app/controllers/branch_comparison_controller.rb
+++ app/controllers/branch_comparison_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'set'
 
 class BranchComparisonController < ApplicationController
-  METRICS_NAMES = { 
+  METRIC_TYPES = { 
     'line' => [['ncloc', 0],
               ['statements', 0],
               ['files', 0],
@@ -16,7 +16,7 @@ class BranchComparisonController < ApplicationController
                 ['minor_violations', -1],
                 ['info_violations', -1],
                 ['violations', -1],
-                ['violations_density', -1]],
+                ['violations_density', 1]],
     'comment' => [['comment_lines', 0],
                   ['comment_lines_density', 0]],
     'duplication' => [['duplicated_lines', -1],
@@ -29,7 +29,7 @@ class BranchComparisonController < ApplicationController
                     ['complexity', -1]],
   }
 
-  def index
+  def result
     base_project_id = params[:id]
     target_project_id = params['target']
     @base_project = Project.by_key(base_project_id)
@@ -39,12 +39,23 @@ class BranchComparisonController < ApplicationController
       return
     end
     @metrics = {}
-    METRICS_NAMES.each_pair do |category, arr|
+    METRIC_TYPES.each_pair do |category, arr|
       @metrics[category] = arr.map do |metric_name, type|
         metric = Metric.by_name(metric_name)
         [metric, type]
       end
     end
+    @metric_layout = [['line', 'comment', 'complexity'],
+                      ['issue', 'duplication']]
+    @base_version_list = self._get_project_versions(@base_project.id)
+    @target_version_list = self._get_project_versions(@target_project.id)
+    @base_version = params['base_version'] ? params['base_version'] : @base_version_list[0]
+    @target_version = params['target_version'] ? params['target_version'] : @target_version_list[0]
+    unless @base_version_list.include?(@base_version) and @target_version_list.include?(@target_version)
+      render :text => "Version not found: base #{@base_version}, target: #{@target_version}"
+    end
+    @measure_data = self._get_measure_data(@base_project.id, @base_version_list[0],
+                                            @target_project.id, @target_version_list[0])
   end
 
   def _get_project_versions(id)
@@ -57,38 +68,37 @@ class BranchComparisonController < ApplicationController
 
   def _get_measure_data(base_project_id, base_project_version, target_project_id, target_project_version)
     data = {}
-    METRICS_NAMES.each_pair do |category, arr|
-      arr.each do |metric_name, type|
+    METRIC_TYPES.each_pair do |category, arr|
+      arr.each do |metric_name, metric_type|
         metric = Metric.by_name(metric_name)
         base_snapshot = self._get_latest_snapshot(base_project_id, base_project_version)
         base_project_measure = base_snapshot.measure(metric)
         target_snapshot = self._get_latest_snapshot(target_project_id, target_project_version)
         target_project_measure = target_snapshot.measure(metric)
 
+        data[metric_name] = {'delta' => nil, 'quality' => nil}
         if base_project_measure and target_project_measure
-          data[metric_name] = { 'base' => base_project_measure.formatted_value,
-                                'target' => target_project_measure.formatted_value}
+          data[metric_name]['base'] = base_project_measure.formatted_value
+          data[metric_name]['target'] = target_project_measure.formatted_value
           if base_project_measure.value.is_a?(Numeric) and target_project_measure.value.is_a?(Numeric)
             if target_project_measure.value == base_project_measure.value
-              data[metric_name]['delta'] = nil
+              data[metric_name]['quality'] = 0
             else
               tmp = (target_project_measure.value - base_project_measure.value).round(1)
+              tmp = tmp.to_i if tmp.to_i == tmp
               data[metric_name]['delta'] = tmp > 0 ? "+#{tmp}" : "#{tmp}"
+              data[metric_name]['quality'] = (tmp > 0 ? 1 : -1) * metric_type
             end
           end
         else
           data[metric_name] = { 'base' => base_project_measure ? base_project_measure.formatted_value : nil,
                                 'target' => target_project_measure ? target_project_measure.formatted_value : nil,
-                                'delta' => nil}
+                                'delta' => nil,
+                                'quality' => nil}
         end
       end
     end
     return data
-  end
-
-  def get_measure_data
-    base_project_id = Project.by_key(params['base'])
-    render :json => self._get_measure_data(params['')
   end
 
   # get latest snapshot of a specific version
@@ -98,118 +108,4 @@ class BranchComparisonController < ApplicationController
                               :order => 'created_at DESC')
     return snapshot
   end
-#  def index
-#    begin
-#    # make sure id/key is specified
-#    unless params[:id]
-#      render :text => 'Usage: /branch_comparison/<base_branch_id_or_key>?target_branches=<branch1>,<branch2>'
-#      return
-#    end
-#    # get target branches
-#    if params.has_key?(:target_branches)
-#      unless params[:target_branches] =~ /[^,]+(,[^,]+)*/i
-#        render :text => 'Multiple branches should be separated by comma'
-#        return
-#      end
-#      target_branch_names = params[:target_branches].split(',')
-#    else
-#      target_branch_names = nil
-#    end
-#    # find base project
-#    @base_project = Project.by_key(params[:id])
-#    if @base_project == nil
-#      render :text => "Project #{params[:id]} doesn't exist"
-#      return
-#    end
-#
-#    # find other branches
-#    @target_projects = self._get_branches(@base_project.key, target_branch_names)
-#    # find enabled metrics
-#    @metrics = Metric.all.select {|metric| metric.enabled}
-#    #@metrics = [Metric.by_id(1)]
-#    @metrics.sort! {|a, b| a.name <=> b.name}
-#    # find all available versions for each project
-#    @versions = {}
-#    [@base_project].concat(@target_projects).map {|project| project.id}.each do |id|
-#      @versions[id] = self._get_project_versions(id)
-#    end
-#    if @versions[@base_project.id].empty?
-#      render :text => "Base project #{@base_project.id} has no snapshots"
-#      return
-#    end
-#    @base_project_measure_data = self._get_measure_data(@base_project,
-#                                                        @versions[@base_project.id][0],
-#                                                        @metrics)
-#    @target_projects_measure_data = {}
-#    @target_projects.each do |project|
-#        @target_projects_measure_data[project.id] = self._get_measure_data(project,
-#                                                                            @versions[project.id][0],
-#                                                                            @metrics)
-#    end
-#
-#    rescue => e
-#      render :text => e.backtrace.join("\n")
-#    end
-#  end
-#
-#
-#  # get the other branches of a project
-#  def _get_branches(project_key, target_branches=nil)
-#    # get project key(without branch name)
-#    project_key_without_branch = project_key.split(':')[0,2].join(':')
-#    sql_template = "kee LIKE :key_pattern AND kee != :base_branch_key
-#                    AND root_id IS NULL"
-#    sql_params = {:key_pattern => "#{project_key_without_branch}%",
-#                  :base_branch_key => project_key}
-#    if target_branches and not target_branches.empty?
-#      sql_template << " AND kee IN (:target_branch_keys)"
-#      sql_params[:target_branch_keys] = target_branches.map do |branch_name|
-#        [project_key_without_branch, branch_name].join(':')
-#      end
-#    end
-#    # filter projects
-#    target_branches = Project.all(:conditions => [sql_template, sql_params],
-#                                  :order => "kee ASC")
-#    target_branches.map! {|item| Project.by_key(item.id)}
-#    return target_branches
-#  end
-#
-#
-#  def _get_measure_data(project, version, metrics)
-#    # find snapshot
-#    snapshot = self._get_latest_snapshot(project.id, version)
-#    if snapshot.nil?
-#      render :json => nil
-#      return
-#    end
-#    # calculate result for each metric
-#    measure_data = {}
-#    metrics.each do |metric|
-#      project_measure = snapshot.measure(metric)
-#      if project_measure
-#        measure_data[metric.id] = [project_measure.formatted_value, project_measure.value]
-#      else
-#        measure_data[metric.id] = [nil, nil]
-#      end
-#    end
-#    data = {'id' => project.id,
-#            'name' => project.name(true),
-#            'version' => version,
-#            'created_at' => snapshot.created_at,
-#            'measure_data' => measure_data}
-#    return data
-#  end
-#
-#  # params[:id]         project id
-#  # params['version']   project version
-#  # params['metrics']   metric ids
-#  def get_measure_data
-#    project = Project.by_key(params[:id])
-#    unless project
-#      render :json => nil
-#    end
-#    metrics = params['metrics'].split(',').map {|id| Metric.by_id(id.to_i)}
-#    data = self._get_measure_data(project, params['version'], metrics)
-#    render :json => data
-#  end
 end
