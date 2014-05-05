511ed2221a2c139fd3275354fb6bff4dbd18a6c5
jizhao <jizhao@redhat.com>
2014-05-05 10:39:51 +0800

    fix bug of sending email

--- src/main/resources/org/sonar/ror/branchcomparison/app/controllers/branch_comparison_controller.rb
+++ src/main/resources/org/sonar/ror/branchcomparison/app/controllers/branch_comparison_controller.rb
@@ -89,7 +89,7 @@ class BranchComparisonController < ApplicationController
     if params['email']
       subject = "[sonar] #{@base_project.name(true)} <=> #{@target_project.name(true)}"
       sender = "noreply@redhat.com"
-      receiver = params['email']
+      receiver = params['email'].strip
       text = "#{"#".ljust(30)}#{@base_project.branch.ljust(25)}#{@target_project.branch.ljust(25)}\n"
       @measure_data.each_pair do |metric_name, data|
         if data['delta']
@@ -101,7 +101,8 @@ class BranchComparisonController < ApplicationController
         else
           delta = ""
         end
-        text << "#{Metric.by_name(metric_name).short_name.ljust(30)}#{data['base'].ljust(25)}#{(data['target'] + delta).ljust(25)}"
+        metric = Metric.by_name(metric_name)
+        text << "#{metric.short_name.ljust(30)}#{data['base'].ljust(25)}#{"#{data['target']}#{delta}".ljust(25)}\n"
       end
       self._send_email(subject, text, sender, receiver)
     end
